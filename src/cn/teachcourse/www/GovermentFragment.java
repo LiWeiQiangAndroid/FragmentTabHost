eb2301879ac20253fc24f43ff698eaa650372204
lwqldsyzx <lwqldsyzx@126.com>
2016-07-19 01:45:11 +0800

    QQ底部导航

---
+++ src/cn/teachcourse/www/GovermentFragment.java
@@ -0,0 +1,21 @@
+package cn.teachcourse.www;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/*
+@author TeachCourse
+@date ´´½¨ÓÚ£º2015-11-4
+ */
+@SuppressLint("NewApi")
+public class GovermentFragment extends Fragment {
+    @Override  
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {        
+        return inflater.inflate(R.layout.goverment_fragment, null);       
+    } 
+}
+
